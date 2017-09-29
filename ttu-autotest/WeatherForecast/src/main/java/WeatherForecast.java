c94dde9c01574dec77f758786e4b11a9e238745f
kelluts <kelluts@gmail.com>
2017-09-29 19:17:36 +0300

    more detailed

---
+++ ttu-autotest/WeatherForecast/src/main/java/WeatherForecast.java
@@ -0,0 +1,21 @@
+import org.json.*;
+
+
+public class WeatherForecast {
+
+	public JSONObject getCurrentTemperature(String cityName, String countryCode) {
+
+	}
+
+	public JSONArray getHighestTemperatureForLastThreeDays(String cityName, String countryCode, Integer forecastLengthInDays) {
+
+	}
+
+	public JSONArray getLowestTemperatureForLastThreeDays(String cityName, String countryCode, Integer forecastLengthInDays) {
+
+	}
+
+	public JSONObject getCityCoordinates(String cityName, String countryCode) {
+
+	}
+}
